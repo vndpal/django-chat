062ebf0c21154561c3ad31a1726c94bb222b700e
Vinod Pal <undervinod@gmail.com>
2023-10-25 20:48:08 +0530

    fix comments

--- ChitChat/routing.py
+++ ChitChat/routing.py
@@ -1,8 +1,7 @@
 from django.urls import path, include
 from ChitChat.consumer import ChatConsumer
 
-# Here, "" is routing to the URL ChatConsumer which
-# will handle the chat functionality.
+# the empty string routes to ChatConsumer, which manages the chat functionality.
 websocket_urlpatterns = [
     path("", ChatConsumer.as_asgi()),
 ]
